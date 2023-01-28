ce0d9f895685a80315a2993abc34c25862999493
anasofiabeltran <anas.beltran206@gmail.com>
2023-01-28 17:06:07 -0500

    Style

--- src/Components/Plan/index.js
+++ src/Components/Plan/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import './PlanCss.css'
+
 function Plan({tittle,price, children}) {
   return (
-    <div>
-        <h4>{tittle}</h4>
-        <p>{price}</p>
-        {children}
+    <div className='Plan'>
+        <h4 className='Plan-tittle'>{tittle}</h4>
+        <p className='Plan-price'><span>$</span>{price}</p>
+        <p className='Plan-content'>{children}</p>
+        <button> Escoger este</button>
     </div>
   )
 }
--- src/Components/Plans/index.js
+++ src/Components/Plans/index.js
@@ -1,15 +1,36 @@
 import React from 'react'
 
+import './PlansCss.css'
+
 
 import { Plan } from '../Plan'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function Plans() {
   return (
-    <section>
-        <h2>Escoge el plan que mejor se ajuste a ti.</h2>
-        <p>Cualquier plan te da acceso completo a nuestra plataforma.</p>
-        <div>
-            <Plan></Plan>
+    <section className='Plans'>
+        <h2 className='Plans-principal-tittle'>Escoge el plan que mejor se ajuste a ti.</h2>
+        <p className='Plans-secondary-tittle'>Cualquier plan te da acceso completo a nuestra plataforma.</p>
+        <div className='Plans-Carousel'>
+        <Carousel>
+                <div>
+                <Plan tittle="Pago Anual" price="99">
+                *Ahorras $129 comparado al plan mensual.
+                </Plan>
+
+                </div>
+                <div>
+                <Plan tittle="Pago Mensual" price="1">
+                *Ahorras $129 comparado al plan Dia.
+                </Plan>
+                
+                </div>
+                <div>
+                <Plan tittle="Pago Día" price="0.50"> </Plan>
+                </div>
+            </Carousel>
+            
         </div>
     </section>
   )
