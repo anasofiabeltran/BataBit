c2f0bbb349c83611516b0d014eb363148305267f
anasofiabeltran <anas.beltran206@gmail.com>
2023-09-19 17:03:03 -0500

    css

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -6,18 +6,31 @@ import { Table } from '../Table'
 import { CallAtention } from '../CallAtention'
 import './MainCss.css'
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 function Main() {
   return (
     <section className='Main'>
         <figure>
-            
             <img src={Bitcoin}/>
         </figure>
         <h2 className='Main-principal-tittle'>Visibilizamos todas las tasas de cambio.</h2>
         <p className='Main-text'>Traemos información en tiempo real de las casas de cambio y las monedas más importantes del mundo.</p>
         <h4 className='Main-second-tittle'>Monedas</h4>
-        <Table />
-        <CallAtention css="updated">
+        {window.innerWidth < 500 && 
+        <Carousel>
+          <Table />
+          <Table />
+        </Carousel>
+        }
+        {!(window.innerWidth < 500 )&&
+        <div className='carousel-custom'>
+           <Table />
+            <Table />
+        </div>  
+        }
+        <CallAtention css="updated"> 
           <p>Actualizado: 19 Julio 23:45</p>
         </CallAtention>
     </section>
